b2796c1b5339a8c888c0155ee710ff51627a3a23
J.Lee <joverlee521@gmail.com>
2018-12-24 15:31:47 -0800

    made navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function NavBar(props){
+    return(
+        <ul className="nav sticky-top justify-content-between align-items-center flex-lg-row flex-column px-3">
+            <li className="nav-item">
+                <img src="/images/my-hero-logo.png" id="nav-img" alt="My Hero Academia"/>
+            </li>
+            <li className="nav-item text-white h3">Click an image to start the game!</li>
+            <li className="nav-item text-white h3">Score: {props.score} | Top Score: {props.topScore}</li>
+        </ul>
+    );
+}
+
+export default NavBar;
