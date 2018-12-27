d38cedb9abe0e3421a8af8c5e43c3d2716defaba
J.Lee <joverlee521@gmail.com>
2018-12-27 12:23:10 -0800

    altered img src url

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -16,7 +16,7 @@ function NavBar(props){
     return(
         <ul className="nav sticky-top justify-content-between align-items-center flex-lg-row flex-column px-3">
             <li className="nav-item">
-                <img src="/images/my-hero-logo.png" id="nav-img" alt="My Hero Academia"/>
+                <img src={`${process.env.PUBLIC_URL}/images/my-hero-logo.png`} id="nav-img" alt="My Hero Academia"/>
             </li>
             <li className="nav-item text-white h3">
                 {message}
