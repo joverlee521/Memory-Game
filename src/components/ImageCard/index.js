5c4d05da741d4e130c8ab0ec653f78844fcc0433
J.Lee <joverlee521@gmail.com>
2018-12-27 12:27:23 -0800

    changed card img src

--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function ImageCard(props){
     return(
         <div className="card character-card" onClick={() => props.clickState(props.character.id)}>
-            <img className="card-img img-custom" src={props.character.image} alt={props.character.name}/>
+            <img className="card-img img-custom" src={`${process.env.PUBLIC_URL}` + props.character.image} alt={props.character.name}/>
         </div>
     );
 }
