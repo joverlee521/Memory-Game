38057b66eb57e363f1e6c4830dd47e0de0d868ad
J.Lee <joverlee521@gmail.com>
2018-12-27 12:00:11 -0800

    Updates

---
+++ precache-manifest.092bed86978cc4d25e184095365a7584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc4f68140219dda8e8d9",
+    "url": "/Memory-Game/static/css/main.e1df651a.chunk.css"
+  },
+  {
+    "revision": "cc4f68140219dda8e8d9",
+    "url": "/Memory-Game/static/js/main.cc4f6814.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Memory-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "c4ec12671d5e632793fda2f4136b6d33",
+    "url": "/Memory-Game/index.html"
+  }
+];
